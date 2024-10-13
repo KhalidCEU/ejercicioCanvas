4f3bf8ab9b12e02323796b904b3a8a2a70f8072c
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-10-13 20:18:02 +0200

    feat: add Rectangle drawing & dynamic figure construction
    This refactor allows us to dinamically pass parameters to the constructor based on the selected figure, used for cleaner code as well

--- script.js
+++ script.js
@@ -5,7 +5,20 @@ canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 class Rectangle{
-    constructor(){
+    constructor(x, y, height, width, color) {
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.color = color
+    }
+
+    draw(ctx) {
+        ctx.beginPath()
+        ctx.closePath();
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x, this.y, this.width, this.height)
+        ctx.fill();
     }
 }
 
@@ -48,8 +61,23 @@ function getRandomColor() {
 function createRandomShape() {
     const x = Math.random() * canvas.width;
     const y = Math.random() * canvas.height;
+    const height = Math.random() * 100;
+    const width = Math.random() * 100 + 10;
     const size = Math.random() * 50 + 10;
     const color = getRandomColor();
-    const triangle = new Triangle(x, y, size, color);
-    triangle.draw(ctx);
+
+    const figures = [
+        ['Triangle', [x, y, size, color]],
+        ['Rectangle', [x, y, height, width, color]]
+    ];
+    const index = Math.floor(Math.random() * figures.length);
+    const [figureType, params] = figures[index];
+
+    const FigureConstructor = {
+        'Rectangle': Rectangle,
+        'Triangle': Triangle,
+    }
+
+    const figure = new FigureConstructor[figureType](...params);
+    figure.draw(ctx);
 }
